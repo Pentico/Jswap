918fde78d9acb8f7060eca212dd6f589728a5926
Pentico <morulaneat@gmail.com>
2017-02-09 11:02:43 +0200

    nav bar edit

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -59,40 +59,34 @@ class Navbar extends React.Component {
 
     render() {
         return (
-            <nav className='navbar navbar-default navbar-static-top'>
-                <div className='navbar-header'>
-                    <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar'>
-                        <span className='sr-only'>Toggle navigation</span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
-                    </button>
-                    <Link to='/' className='navbar-brand'>
-                     <span ref='triangles' className={'triangles animated ' + this.state.ajaxAnimationClass}>
-              <div className='tri invert'></div>
-              <div className='tri invert'></div>
-              <div className='tri'></div>
-              <div className='tri invert'></div>
-              <div className='tri invert'></div>
-              <div className='tri'></div>
-              <div className='tri invert'></div>
-              <div className='tri'></div>
-              <div className='tri invert'></div>
-            </span>
-                <div  className="Name">
-                        Jswap
-                        <span className='badge badge-up badge-danger'>{this.state.onlineUsers}</span>
-                </div>
-              </Link>
-          </div>
-                <div id='navbar' className='navbar-collapse collapse'>
-                    <ul className='nav navbar-nav'>
-                        <li><a onClick={this.handleClickEvent.bind(this,0)}>Home</a></li>
-                        <li><a onClick={this.handleClickEvent.bind(this,1)}>Bids</a></li>
-                        <li><a onClick={this.handleClickEvent.bind(this,2)}>My Account</a></li>
-                        <li><a onClick={this.handleClickEvent.bind(this,3)}>Add Item</a></li>
-                    </ul>
-                    
+            <nav className='navbar navbar-default navbar-fixed-top'>
+                <div className='container'>
+                    <div className='navbar-header'>
+                        <button className='navbar-toggle' type='button'data-toggle='collapse' data-target='.navbar-collapse'>
+                            <span className='sr-only'>Toggle navigation</span>
+                            <span className='icon-bar'></span>
+                            <span className='icon-bar'></span>
+                            <span className='icon-bar'></span>
+                        </button>
+                        <Link to='/' className='navbar-brand'>
+                            <i className='fa fa-cube'/>
+                            Jswap
+                        </Link>
+                    </div>
+                        <div className='collapse navbar-collapse'>
+                            <ul className='nav navbar-nav'>
+                                <li><a onClick={this.handleClickEvent.bind(this,0)}>Home</a></li>
+                                <li><a onClick={this.handleClickEvent.bind(this,1)}>Bids</a></li>
+                                <li><a onClick={this.handleClickEvent.bind(this,4)}>Contacts</a></li>
+                                <li><a onClick={this.handleClickEvent.bind(this,5)}>About</a></li>      
+                            </ul>
+
+                            <ul className='nav navbar-nav navbar-right'>
+                                 <li><a onClick={this.handleClickEvent.bind(this,3)}>Add Item</a></li>
+                                 <li><a onClick={this.handleClickEvent.bind(this,2)}>My Account</a></li>
+                                 <li><a onClick={this.handleClickEvent.bind(this,6)}>Login</a></li>
+                            </ul>
+                      </div>
                 </div>
             </nav>
         );
@@ -105,3 +99,41 @@ export default Navbar;
 //                         handleLogout={this.handleLogout.bind(this)} handleSubmitSignUp={this.handleSubmitSignUp.bind(this)}
 //                          />
 //                     <a onClick={NavbarActions.loginSign} >{this.state.message0}</a>
+
+
+    //   <nav className='navbar navbar-default navbar-fixed-top'>
+    //             <div className='navbar-header'>
+    //                 <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar'>
+    //                     <span className='sr-only'>Toggle navigation</span>
+    //                     <span className='icon-bar'></span>
+    //                     <span className='icon-bar'></span>
+    //                     <span className='icon-bar'></span>
+    //                 </button>
+    //                 <Link to='/' className='navbar-brand'>
+    //                  <span ref='triangles' className={'triangles animated ' + this.state.ajaxAnimationClass}>
+    //           <div className='tri invert'></div>
+    //           <div className='tri invert'></div>
+    //           <div className='tri'></div>
+    //           <div className='tri invert'></div>
+    //           <div className='tri invert'></div>
+    //           <div className='tri'></div>
+    //           <div className='tri invert'></div>
+    //           <div className='tri'></div>
+    //           <div className='tri invert'></div>
+    //         </span>
+    //             <div  className="Name">
+    //                     Jswap
+    //                     <span className='badge badge-up badge-danger'>{this.state.onlineUsers}</span>
+    //             </div>
+    //           </Link>
+    //       </div>
+    //             <div id='navbar' className='navbar-collapse collapse'>
+    //                 <ul className='nav navbar-nav'>
+    //                     <li><a onClick={this.handleClickEvent.bind(this,0)}>Home</a></li>
+    //                     <li><a onClick={this.handleClickEvent.bind(this,1)}>Bids</a></li>
+    //                     <li><a onClick={this.handleClickEvent.bind(this,2)}>My Account</a></li>
+    //                     <li><a onClick={this.handleClickEvent.bind(this,3)}>Add Item</a></li>
+    //                 </ul>
+                    
+    //             </div>
+    //         </nav>
