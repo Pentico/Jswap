375cdfebf8ff8ce7b1b454feb4c18b8efeb89785
Pentico <morulaneat@gmail.com>
2017-02-13 16:10:27 +0200

    Sign Up

--- Routes/User.js
+++ Routes/User.js
@@ -33,25 +33,6 @@ router.delete('/delete', function (req, res) {
     /*res.render('users', {title:"User removed"});*/
 }); // EOF
 
-// Login in
-// router.post('/login',
-//     passport.authenticate('local-login', {
-//         successRedirect:'/',
-//         failureRedirect:'/Login',
-//         failureFlash : true
-// })); // EOF
-
-// router.post('/login',
-//   passport.authenticate('local-login'),
-//   function(req, res) {
-//     // If this function gets called, authentication was successful.
-//     // `req.user` contains the authenticated user.
-//     res.send({
-//         message:true,
-//         User:req.user
-//     });
-//   });
-
   router.post('/login', function(req, res, next) {
     passport.authenticate('local-login', function(err, user, info) {
         if (err) { return next(err); }
@@ -67,45 +48,41 @@ router.delete('/delete', function (req, res) {
     })(req, res, next);
 });
 
-// router.post('/login', function(req, res,next){
-//     passport.authenticate('local-login',function(err, user, info){
-//         if(err) { return next(err);}
 
-//         if(!user) {
-//             return res.send({message:user});
-            
-//         }
+/**
+ * Sign Up API
+ */
+router.post('/signUp', function(req, res, next) {
+    passport.authenticate('local-signup', function(err, user, info) {
+        if (err) { return next(err); }
+        if (!user) { return res.send({
+            message : false
+        }); }
+        req.logIn(user, function(err) {
+        if (err) { return next(err); }
+        return res.send({
+            message:true
+        });
+        });
+    })(req, res, next);
+}); // EOF
 
-//         req.logIn(user, function(err) {
-//             if(err) {
-//                 return next(err);
-//             }
-//             res.json({
-//             success: true,
-//             message: 'You have successfully logged in!',
-//             token,
-//             user: userData
-//             });
-//         });
-//     })(req, res, next);
-// }); // EOF
 
 
 //TODO make this send a message to front weather it was successful or not
-router.post('/signUp', passport.authenticate('local-signup',{
+// router.post('/signUp', passport.authenticate('local-signup',{
 
-    successRedirect : '/', // redirect to the secure profile section
-    failureRedirect : '/SignUp', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
+//     successRedirect : '/', // redirect to the secure profile section
+//     failureRedirect : '/SignUp', // redirect back to the signup page if there is an error
+//     failureFlash : true // allow flash messages
 
-})); // EOF
+// })); // EOF
 
 /**
  * authenticate  
  */
   router.post('/authentication', function(req,res,next){
 
-      console.log('In authenticate');
       if (req.user){
             res.send({
                 message:true
